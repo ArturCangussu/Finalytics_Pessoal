75dfa9493c5e8b45cb2a2bb913599904a2deb722
Artur <arturcangussubt@gmail.com>
2025-07-04 08:58:08 -0300

    Commitando Subtópicos

--- Filehandling.py
+++ Filehandling.py
@@ -11,6 +11,19 @@ df_filtrado['Topico'] = np.where(df_filtrado['Tipo de Pix'] == 'Enviado', 'Despe
 
 
 regras_de_categorizacao = {
-    ''
+    'MARCOS VINICIUS DOS SANTOS BARRETO': 'Manutencao',
+    'KAIROS': 'Servico administrativo',
+    'IVONETE VIANA DE SOUSA': 'Despesa Mensal',
+    'AGUINALDO DIAS DO CARMO': 'Despesa Mensal',
+    'FABIANA CARDOSO DOS SANTOS VAZ': 'Despesa Mensal'
 }
-print(df_filtrado)
+
+def categorizar_transacao(descricao):
+    if not isinstance(descricao, str):
+        return 'Descrição Inválida'
+    for palavra_chave, categoria in regras_de_categorizacao.items():
+        if palavra_chave.lower() in descricao.lower():
+            return categoria
+    return 'Nao categorizado'
+
+df_filtrado['Subtopico'] = df_filtrado['Remetente/Destinatario'].apply(categorizar_transacao)
