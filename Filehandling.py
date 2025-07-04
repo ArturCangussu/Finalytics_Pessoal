4a7a61d06c137ea236d0bdd696759d13d73ef1b1
Artur <arturcangussubt@gmail.com>
2025-07-04 08:08:40 -0300

    Filtrando o df

--- Filehandling.py
+++ Filehandling.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_excel('Extrato_pix.xlsx')
-print(df.head())
+
+df_filtrado = df[df['Situacao'] == 'EFETIVADA']
+
+df_erro = df[df['Situacao'] != 'EFETIVADA']
+
+df_filtrado['Topico'] = np.where(df_filtrado['Tipo de Pix'] == 'Enviado', 'Despesa', 'Receita')
+
+
+regras_de_categorizacao = {
+    ''
+}
+print(df_filtrado)
