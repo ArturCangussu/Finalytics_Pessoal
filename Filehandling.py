82e54ce934ecb4a5fb3de6ccdb5724d23c018bb9
Artur <arturcangussubt@gmail.com>
2025-07-16 10:04:20 -0300

    Aplicação do tipo decimal, e separando as despesas por topico

--- Filehandling.py
+++ Filehandling.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from decimal import Decimal
 
 df = pd.read_excel('Extrato_pix.xlsx')
 
@@ -14,19 +15,25 @@ df_filtrado = df[df['Situacao'] == 'EFETIVADA']
 
 # define que o que não for efetivado será guardado em outro df
 df_erro = df[df['Situacao'] != 'EFETIVADA']
+# Garante que o df_filtrado seja uma cópia independente
+df_filtrado = df_filtrado.copy()
+
+# Converte a coluna 'Valor' de string com R$ e vírgula para Decimal com precisão
+def limpar_valor(v):
+    try:
+        v = str(v)
+        v = v.replace('R$', '').replace('\xa0', '').replace(',', '.').strip()
+        return Decimal(v)
+    except:
+        return Decimal('0.00')  
+
+df_filtrado['Valor'] = df_filtrado['Valor'].apply(limpar_valor)
+
 
 # se o tipo de pix for enviado = despesa, se não, receita
 df_filtrado['Tópico'] = np.where(
     df_filtrado['Tipo de Pix'] == 'Enviado', 'Despesa', 'Receita')
 
-
-# regras_de_categorizacao = {
-#   'MARCOS VINICIUS DOS SANTOS BARRETO': 'Manutenção',
-#  'KAIROS': 'Serviço administrativo',
-# 'IVONETE VIANA DE SOUSA': 'Despesa Mensal',
-# 'AGUINALDO DIAS DO CARMO': 'Despesa Mensal',
-# 'FABIANA CARDOSO DOS SANTOS VAZ': 'Despesa Mensal'
-# }
 
 def categorizar_transacao(descricao):
     if not isinstance(descricao, str):
@@ -37,10 +44,25 @@ def categorizar_transacao(descricao):
     return 'Não categorizado'
 
 
+
 df_filtrado['Subtópico'] = df_filtrado['Remetente/Destinatario'].apply(
     categorizar_transacao)
 
 df_receitas = df_filtrado[df_filtrado['Tópico'] == 'Receita']
 df_despesas = df_filtrado[df_filtrado['Tópico'] == 'Despesa']
 
-df_receitas['Valor'].sum()
+total_despesas = df_despesas['Valor'].sum()
+total_receitas = df_receitas['Valor'].sum()
+saldo_liquido = total_receitas - total_despesas
+
+#separando as despesas por topico
+resumo_despesas = df_despesas.groupby('Subtópico')['Valor'].sum().sort_values()
+
+#deixando bonito pra printar
+print("\n--- Detalhamento de Despesas por Categoria ---")
+resumo_formatado = resumo_despesas.abs().astype(float).to_string(
+    header=False,
+    float_format='R$ {:>10,.2f}'.format
+)
+
+print(resumo_despesas)
