cb60a18ac23bd8b93d1cba2fab578022c6c1382c
Artur <arturcangussubt@gmail.com>
2025-07-17 21:13:49 -0300

    liga o html para entregar ao

--- analisador/views.py
+++ analisador/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-# Esta é a nossa primeira view.
-# Toda view recebe um 'request' como primeiro argumento.
+# Não precisamos mais do HttpResponse aqui, então podemos remover.
+
 def pagina_inicial(request):
-    # A view deve retornar uma 'HttpResponse'.
-    # Aqui, estamos retornando um texto simples em HTML.
-    return HttpResponse("<h1>Analisador Financeiro</h1><p>Bem-vindo à página inicial do seu projeto!</p>")
+    # Trocamos HttpResponse por render.
+    # O render precisa do 'request' e do caminho para o arquivo HTML.
+    return render(request, 'analisador/pagina_inicial.html')
