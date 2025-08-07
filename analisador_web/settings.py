b6bb9ba6bc8d2a1b9378728b463fca83fae7dd45
Artur <arturcangussubt@gmail.com>
2025-08-07 11:28:12 -0300

    f

--- analisador_web/settings.py
+++ analisador_web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pofh90ub&cq!#n66*cd0)k-#+z%yy3xm!#scvh4j3p%3asih7h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['arturcangussu.pythonanywhere.com', '127.0.0.1', 'localhost']
 
